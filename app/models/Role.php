6fec6d0be40420a0f0e1acfb1a091f36048dfe4c
Argenis Gutierrez <argenis.v.gtz@gmail.com>
2025-04-03 21:30:13 -0600

    feat:role model getAllRoles func
    se agrego el modelo de roles solo con la funcion para traer todos los
    roles de la base de datos

--- app/models/Role.php
+++ app/models/Role.php
@@ -1,17 +1,36 @@
 <?php
+/* ========================================================
+ * ============ Modelo de Roles ======================
+ * ======================================================*/
 
 namespace App\Models;
 
 class Role extends Conexion
 {
+    protected $table = 'roles';
+
     public function getAllRoles()
     {
         try {
-            $stmt = $this->getConexion()->query("SELECT * FROM roles");
+            $stmt = $this->getConexion()->query("SELECT * FROM {$this->table}");
             return $stmt->fetchAll(\PDO::FETCH_ASSOC);
         } catch (\Throwable $th) {
             return [];
         }
     }
 
+    public function createRole($data)
+    {
+        extract($data);
+        try{
+            $stmt = $this->getConexion()->prepare("INSERT INTO roles ( nombre_rol,fyh_creacion,estado ) VALUES (:nombre_rol,:fyh_creacion,:estado)");
+            $stmt->bindParam('nombre_rol', $nombre_rol);
+            $stmt->bindParam('fyh_creacion', $fechaHora);
+            $stmt->bindParam('estado', $estado);
+            $stmt->execute();
+            return true;
+        }catch(\Throwable $th){
+            return false;
+        }
+    }
 }
