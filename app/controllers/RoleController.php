eb933043e3836fc2f83eb1fa2bbe381d077d532c
Argenis Gutierrez <argenis.v.gtz@gmail.com>
2025-04-07 14:00:26 -0600

    feat:Role controller function index
    se creo la funcion para recibir los datos de la db en la vista principal
    de los roles

--- app/controllers/RoleController.php
+++ app/controllers/RoleController.php
@@ -4,14 +4,18 @@
  * ======================================================*/
 
 namespace App\Controllers;
+use App\Models\Role;
 
 class RoleController extends Controller
 {
     public function index()
     {
+        $rolesModel = new Role();
         return $this->view(
-            'roles', [
-            ]
+            'roles', ['roles'=>$rolesModel->getAllRoles()]
         );
+    }
+    public function create()
+    {
     }
 }
