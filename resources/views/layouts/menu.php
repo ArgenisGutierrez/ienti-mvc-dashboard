8774733fd786b484bc88bef5a97acb05a312cb56
Argenis Gutierrez <argenis.v.gtz@gmail.com>
2025-04-14 12:40:29 -0600

    feat:create laouts views
    se crearon layouts para las cosas que se repiten en cada vista como el
    header, el menu y el footer

---
+++ resources/views/layouts/menu.php
@@ -0,0 +1,36 @@
+
+      <nav class="sidebar-wrapper">
+
+        <!-- Sidebar menu starts -->
+        <div class="sidebar-menu">
+          <div class="sidebarMenuScroll">
+            <ul>
+              <li>
+                <a href="<?php echo APP_URL;?>">
+                  <i class="bi bi-house"></i>
+                  <span class="menu-text">Home</span>
+                </a>
+              </li>
+              <li>
+                <a href="<?php echo APP_URL;?>usuarios">
+                  <i class="bi bi-person-video2"></i>
+                  <span class="menu-text">Usuarios</span>
+                </a>
+              </li>
+              <li>
+                <a href="<?php echo APP_URL;?>recursos">
+                  <i class="bi bi-boxes"></i>
+                  <span class="menu-text">Recursos</span>
+                </a>
+              </li>
+              <li>
+                <a href="<?php echo APP_URL;?>roles">
+                  <i class="bi bi-file-earmark-lock2-fill"></i>
+                  <span class="menu-text">Roles</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <!-- Sidebar menu ends -->
+      </nav>
