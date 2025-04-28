787c3ebe89849bd486b659b5b507cf40e6016162
Argenis Gutierrez <argenis.v.gtz@gmail.com>
2025-04-28 12:08:01 -0600

    style:eliminar notificaciones
    se eleiminaron las notificiaciones y mensajes aun no implementados

--- resources/views/layouts/head.php
+++ resources/views/layouts/head.php
@@ -2,9 +2,9 @@
 
   <!-- Sidebar brand starts -->
   <div class="brand">
-    <a href="<?php echo APP_URL;?>" class="logo">
-      <img src="<?php echo APP_URL;?>images/logo.webp" class="d-none d-md-block me-4" alt="Bloom Admin Dashboard" />
-      <img src="<?php echo APP_URL;?>images/logo.webp" class="d-block d-md-none me-4" alt="Bloom Admin Dashboard" />
+    <a href="<?php echo APP_URL; ?>" class="logo">
+      <img src="<?php echo APP_URL; ?>images/logo.webp" class="d-none d-md-block me-4" alt="Bloom Admin Dashboard" />
+      <img src="<?php echo APP_URL; ?>images/logo.webp" class="d-block d-md-none me-4" alt="Bloom Admin Dashboard" />
     </a>
   </div>
   <!-- Sidebar brand ends -->
@@ -17,49 +17,6 @@
   <div class="header-actions-container">
 
     <!-- Header actions start -->
-    <div class="header-actions d-xl-flex d-lg-none gap-4">
-      <div class="dropdown">
-        <a class="dropdown-toggle" href="#!" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-          <i class="bi bi-envelope-open fs-5 lh-1"></i>
-          <span class="count-label"></span>
-        </a>
-        <div class="dropdown-menu dropdown-menu-end shadow-lg">
-          <div class="dropdown-item">
-            <div class="d-flex py-2 border-bottom">
-              <img src="<?php echo APP_URL?>images/user.png" class="img-3x me-3 rounded-3" alt="Admin Dashboards" />
-              <div class="m-0">
-                <h6 class="mb-1 fw-semibold">Sophie Michiels</h6>
-                <p class="mb-1">Membership has been ended.</p>
-                <p class="small m-0 text-secondary">Today, 07:30pm</p>
-              </div>
-            </div>
-          </div>
-          <div class="dropdown-item">
-            <div class="d-flex py-2 border-bottom">
-              <img src="<?php echo APP_URL?>images/user2.png" class="img-3x me-3 rounded-3" alt="Admin Dashboards" />
-              <div class="m-0">
-                <h6 class="mb-1 fw-semibold">Benjamin Michiels</h6>
-                <p class="mb-1">Congratulate, James for new job.</p>
-                <p class="small m-0 text-secondary">Today, 08:00pm</p>
-              </div>
-            </div>
-          </div>
-          <div class="dropdown-item">
-            <div class="d-flex py-2">
-              <img src="<?php echo APP_URL?>images/user1.png" class="img-3x me-3 rounded-3" alt="Admin Dashboards" />
-              <div class="m-0">
-                <h6 class="mb-1 fw-semibold">Jehovah Roy</h6>
-                <p class="mb-1">Lewis added new schedule release.</p>
-                <p class="small m-0 text-secondary">Today, 09:30pm</p>
-              </div>
-            </div>
-          </div>
-          <div class="d-grid mx-3 my-1">
-            <a href="javascript:void(0)" class="btn btn-primary">View all</a>
-          </div>
-        </div>
-      </div>
-    </div>
     <!-- Header actions start -->
 
     <!-- Header profile start -->
@@ -67,17 +24,17 @@
       <div class="dropdown">
         <a href="#" id="userSettings" class="user-settings" data-toggle="dropdown" aria-haspopup="true">
           <span class="user-name d-none d-md-block">
-            <?php echo $_SESSION['nombre']?>
+            <?php echo $_SESSION['nombre'] ?>
           </span>
           <span class="avatar">
-            <img src="<?php echo !empty($_SESSION['imagen']) ? 'files/'.$_SESSION['imagen'] : 'images/users.svg'; ?>" alt="User Avatar" />
+            <img src="<?php echo !empty($_SESSION['imagen']) ? 'files/' . $_SESSION['imagen'] : 'images/users.svg'; ?>" alt="User Avatar" />
             <span class="status online"></span>
           </span>
         </a>
         <div class="dropdown-menu dropdown-menu-end" aria-labelledby="userSettings">
           <div class="header-profile-actions">
-            <a href="<?php echo APP_URL;?>usuario">Perfil</a>
-            <a href="<?php echo APP_URL;?>logout">Cerrar Sesion</a>
+            <a href="<?php echo APP_URL; ?>usuario">Perfil</a>
+            <a href="<?php echo APP_URL; ?>logout">Cerrar Sesion</a>
           </div>
         </div>
       </div>
--- resources/views/layouts/header.php
+++ resources/views/layouts/header.php
@@ -4,31 +4,11 @@
               <i class="bi bi-house"></i>
             </div>
             <div class="page-title d-none d-md-block">
-            <h5>Bienvenido <?php echo $_SESSION['nombre']?>
+              <h5>Bienvenido <?php echo $_SESSION['nombre'] ?>
               </h5>
             </div>
           </div>
           <!-- Live updates start -->
-          <ul class="updates d-flex align-items-end flex-column overflow-hidden" id="updates">
-            <li>
-              <a href="javascript:void(0)">
-                <i class="bi bi-envelope-paper text-red font-1x me-2"></i>
-                <span>12 emails from David Michaiah.</span>
-              </a>
-            </li>
-            <li>
-              <a href="javascript:void(0)">
-                <i class="bi bi-bar-chart text-blue font-1x me-2"></i>
-                <span>15 new features updated successfully.</span>
-              </a>
-            </li>
-            <li>
-              <a href="javascript:void(0)">
-                <i class="bi bi-folder-check text-yellow font-1x me-2"></i>
-                <span>The media folder is created successfully.</span>
-              </a>
-            </li>
-          </ul>
           <!-- Live updates end -->
 
         </div>
